ab480e72c59b0730eef7ade27cb3aff7a2364d4b
ribinmathew <ribinmathew4u@gmail.com>
2023-08-13 11:14:52 +0530

    change the cors settings

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ import userRouter from './src/routes/userRoutes.js';
 import helmet from 'helmet';
 dotenv.config();
 import morgan from "morgan";
-// import errorHandler from "./src/middlewares/error/errorHandler.js";
-// import cors from "./src/middlewares/security/cors.js";
 import connectDatbase from "./src/config/database.js"
 
 const app = express();
@@ -18,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:5173']
+  origin: ['http://localhost:5173','https://safelockbackend.onrender.com']
 }));
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
@@ -27,7 +25,6 @@ connectDatbase();
 
 app.use("/api/v1", userRouter);
 
-// app.use(errorHandler);
 
 app.use((req, res) => {
   res.status(404).json({ success: false, status: 404, message: "Not found" });
